3b931e35a2433425474932a8735ee46b6185e040
Shreyas S Kasetty <shreyasskasetty@MacBook-Pro-58.local>
2023-10-17 02:04:43 -0500

    add Procfile and small changes

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,32 +1,40 @@
 import React, { useState } from 'react';
-import { Container, Typography, TextField, Button, Box } from '@mui/material';
+import { Container, Typography, TextField, Button, Box, Select, MenuItem } from '@mui/material';
 import axios from 'axios';
 import { BallTriangle } from 'react-loader-spinner';
+
+const colorMaps = ['viridis', 'plasma', 'inferno', 'magma', 'cividis', 'cool', 'hot', 'coolwarm'];
 
 const LandingPage = () => {
   const [documentId, setDocumentId] = useState('');
   const [image_url, setImageUrl] = useState('');
-  const [isLoading, setIsLoading] = useState(false); 
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedColorMap, setSelectedColorMap] = useState('viridis'); // Default color map
+
   const handleDocumentIdChange = (event) => {
     setDocumentId(event.target.value);
+  };
+
+  const handleColorMapChange = (event) => {
+    setSelectedColorMap(event.target.value);
   };
 
   const handleVisualization = async () => {
     setIsLoading(true);
     try {
-        // Send a POST request with the documentId in the request body
-        const response = await axios.post(`http://127.0.0.1:5001/api/v1/shadow_analysis/visualize-shadow-data`, {
-          document_id: documentId,
-          color_map: "viridis"
-        });
-  
-        const image_url = response.data; // Assuming your API response contains timestamp and sh
-        console.log(response)
-        setImageUrl(image_url);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+      // Send a POST request with the documentId and selectedColorMap in the request body
+      const response = await axios.post(`http://ec2-35-173-183-116.compute-1.amazonaws.com:5001/api/v1/shadow_analysis/visualize-shadow-data`, {
+        document_id: documentId,
+        colormap: selectedColorMap,
+      });
+
+      const image_url = response.data; // Assuming your API response contains the image URL
+      setImageUrl(image_url);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -35,22 +43,21 @@ const LandingPage = () => {
         Shadow Analysis Visualization
       </Typography>
       <Box display="flex" flexDirection="column" alignItems="center">
-      {isLoading ? (
-          <BallTriangle // Choose the type of spinner animation
-            color="#3498db" // Customize the spinner color
-            height={50} // Set the spinner height
-            width={50} // Set the spinner width
+        {isLoading ? (
+          <BallTriangle
+            color="#3498db"
+            height={50}
+            width={50}
           />
-        ):(
+        ) : (
           <div style={{ width: '100%', height: '400px', border: '1px solid #ccc' }}>
-          <img
-            src={image_url}
-            alt="Shadow Map"
-            style={{ maxWidth: '100%', maxHeight: '100%', display: 'block', margin: 'auto' }}
-          />
+            <img
+              src={image_url}
+              alt="Shadow Map"
+              style={{ maxWidth: '100%', maxHeight: '100%', display: 'block', margin: 'auto' }}
+            />
           </div>
-        )
-        }
+        )}
         {/* Document ID Input */}
         <TextField
           label="Enter MongoDB Document ID"
@@ -60,6 +67,19 @@ const LandingPage = () => {
           fullWidth
           margin="normal"
         />
+        {/* Color Map Selection */}
+        <Select
+          label="Select Color Map"
+          value={selectedColorMap}
+          onChange={handleColorMapChange}
+          style={{ marginTop: '16px', width: '200px' }}
+        >
+          {colorMaps.map((map) => (
+            <MenuItem key={map} value={map}>
+              {map}
+            </MenuItem>
+          ))}
+        </Select>
         {/* Visualization Buttons */}
         <Button
           variant="contained"
